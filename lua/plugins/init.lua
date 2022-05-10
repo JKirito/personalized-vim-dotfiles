30842910cdd3fc60781860be6fbaee190de96536
JKirito <byrxarpitsingh@gmail.com>
2022-05-10 05:46:58 +0530

    Added treesitter,lualine,nord(theme)

---
+++ lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'shaunsingh/nord.nvim'
+  use {'nvim-treesitter/nvim-treesitter',run = ':TSUpdate'}
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+end)
